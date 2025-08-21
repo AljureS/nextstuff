32be8bfd04e6ac29e8dce682959c4ecefbde9081
AljureS <saidsimon2@gmail.com>
2025-08-20 19:58:29 -0500

    Server and client side rendering

--- next15-store/src/app/page.tsx
+++ next15-store/src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola mundo pagina de incio');
+  
   return (
     <div className={styles.page}>
       <main className={styles.main}>
--- said-store/src/app/page.tsx
+++ said-store/src/app/page.tsx
@@ -2,6 +2,8 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola mundo pagina de incio');
+  
   return (
     <div className={styles.page}>
       <main className={styles.main}>
