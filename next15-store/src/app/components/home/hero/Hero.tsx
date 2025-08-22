92b9f91bc3c43b96a3dc2e33fb200fb41d01cb46
AljureS <saidsimon2@gmail.com>
2025-08-21 21:51:54 -0500

    css modules

--- next15-store/src/app/components/home/hero/Hero.tsx
+++ next15-store/src/app/components/home/hero/Hero.tsx
@@ -1,7 +1,9 @@
+import styles from './Hero.module.css';
 export const Hero = () => {
     return (
-        <section>
+        <section className={styles.Hero}>
             <h1>Welcome to Said Store</h1>
+            <h2>zungita talla M</h2>
         </section>
     )
 }
--- said-store/src/app/components/home/hero/Hero.tsx
+++ said-store/src/app/components/home/hero/Hero.tsx
@@ -1,7 +1,11 @@
+import styles from './Hero.module.css';
 export const Hero = () => {
+    console.log(styles);
+    
     return (
-        <section>
+        <section className={styles.Hero}>
             <h1>Welcome to Said Store</h1>
+            <h2>zungita talla M</h2>
         </section>
     )
 }
