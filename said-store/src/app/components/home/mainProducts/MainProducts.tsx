9a584986f69d3e52921cd0260e24bbaa3a6a8b70
AljureS <saidsimon2@gmail.com>
2025-10-28 21:05:00 -0500

    just 15 version

--- said-store/src/app/components/home/mainProducts/MainProducts.tsx
+++
@@ -1,7 +0,0 @@
-export const MainProducts = () => {
-    return (
-        <section>
-            <h1>Products</h1>
-        </section>
-    )
-}
--- said-store/src/app/store/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div>
-            <nav>Browse all categories</nav>
-            {children} 
-        </div>
-    );
-}
